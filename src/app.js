3c0ce93bc6ac3cf22b519c952671fc4c6f005877
Rubenleyend <rubenmartinezamez@gmail.com>
2025-08-06 09:29:58 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+  const palos = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function elementoAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+// devuelve un nodeList. obligado a iterar para que funcione
+  const palosHTML = document.querySelectorAll(".palo");
+
+  const numero = document.querySelector(".numero");
+
+  const numeroAleatorio = elementoAleatorio(numeros);
+  const paloAleatorio = elementoAleatorio(palos);
+
+  numero.textContent = numeroAleatorio;
+
+  palosHTML.forEach(p => {
+    const tieneRotacion = p.classList.contains("rotated");
+    p.textContent = paloAleatorio;
+    p.className = "palo lh-1 ms-2";
+    
+    if (tieneRotacion) p.classList.add("rotated");
+
+    if (paloAleatorio === "♦" || paloAleatorio === "♥") {
+      p.classList.add(paloAleatorio);
+    }
+  });
 };
+
+
+
+
+
+
+
